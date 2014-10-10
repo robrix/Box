36f766398e2abdbea0f45e73d9b3ecde72181adc
Rob Rix <rob.rix@github.com>
2014-10-10 15:18:19 -0400

    Documentation.

--- Box/BoxType.swift
+++ Box/BoxType.swift
@@ -1,7 +1,13 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+/// The type conformed to by all `Box`es.
 public protocol BoxType {
+	/// The type of the wrapped value.
 	typealias Value
+
+	/// Initializes an intance of the type with a value.
 	init(_ value: Value)
+
+	/// The wrapped value.
 	var value: Value { get }
 }
