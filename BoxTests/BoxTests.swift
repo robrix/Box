3e075055560ba92a40fefce3d7c9573e891021ac
Rob Rix <rob.rix@github.com>
2014-10-10 15:00:08 -0400

    Simple sanity test of Box.

--- BoxTests/BoxTests.swift
+++ BoxTests/BoxTests.swift
@@ -1,5 +1,11 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+import Box
 import XCTest
 
-class BoxTests: XCTestCase {}
+class BoxTests: XCTestCase {
+	func testBox() {
+		let box = Box(1)
+		XCTAssertEqual(box.value, 1)
+	}
+}
