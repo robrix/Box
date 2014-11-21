cc8f68c3eea5d558986ae4b962f738f0b3143ddf
Rob Rix <rob.rix@github.com>
2014-11-21 17:42:50 -0500

    MutableBox is Printable.

--- Box/MutableBox.swift
+++ Box/MutableBox.swift
@@ -5,7 +5,7 @@
 /// While this, like `Box<T>` could be used to work around limitations of value types, it is much more useful for sharing a single mutable value such that mutations are shared.
 ///
 /// As with all mutable state, this should be used carefully, for example as an optimization, rather than a default design choice. Most of the time, `Box<T>` will suffice where any `BoxType` is needed.
-public final class MutableBox<T>: MutableBoxType {
+public final class MutableBox<T>: MutableBoxType, Printable {
 	/// Initializes a `MutableBox` with the given value.
 	public init(_ value: T) {
 		self.value = value
@@ -17,5 +17,11 @@ public final class MutableBox<T>: MutableBoxType {
 	/// Constructs a new MutableBox by transforming `value` by `f`.
 	public func map<U>(f: T -> U) -> MutableBox<U> {
 		return MutableBox<U>(f(value))
+	}
+
+	// MARK: Printable
+
+	public var description: String {
+		return toString(value)
 	}
 }
